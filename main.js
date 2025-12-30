b516ffbb2a5344d26b99975679312b4500f7a664
delusions <mogusblaster@gmail.com>
2025-12-30 22:34:34 +0000

    port change

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ import { startHttpServer, startWebSocketServer } from "./utils.js";
 import instance_name from "./config.js";
 const custom = gradient(["#ff5f6d", "#ff71a0ff"]);
 console.log(custom(`maelink gen2 server [ ${instance_name} ] | Closed beta official (stable) build`));
-startWebSocketServer({ port: 8080 });
-startHttpServer({ port: 6060 });
+startWebSocketServer({ port: 4962 });
+startHttpServer({ port: 2494 });
