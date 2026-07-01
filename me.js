bea2afe3ae06f9f2eeb9a7a734376675cf02a845
Scratchy2 <152662914+Scratchy2@users.noreply.github.com>
2026-07-02 00:23:45 +0500

    Remove world's worst bad practice

--- me.js
+++ me.js
@@ -26,7 +26,8 @@ export async function fetchUser(token, userId) {
 
     return { username: user[0], pfp: user[1], bio: user[2], followers };
   } catch (e) {
-    throw e;
+    console.log(e);
+    return false;
   }
 }
 export async function editUser(token, username, pfp, bio) {
