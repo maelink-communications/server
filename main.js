2e3ad5a8da3109ce2e0db1b1211e2f0caedc0242
Scratchy2 <152662914+Scratchy2@users.noreply.github.com>
2026-07-10 07:33:57 +0500

    Remove Access-Control-Allow-Origin from CORS headers
    Removed Access-Control-Allow-Origin header from CORS configuration.

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ const ALLOWED_URLS = [
 ]
 
 const CORS_HEADERS = {
-  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "content-type, Authorization, p, x-server-id",
   "Access-Control-Allow-Credentials": true,
